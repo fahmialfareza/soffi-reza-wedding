b9d6c4c9f7c9a15e408b812970eafe7ac87e855b
santoadji21 <ahmad.aji.santoso21@gmail.com>
2022-10-23 18:12:56 +0700

    feat: add modal ucapan & gift

---
+++ frontend/components/icons/GiftBox/index.tsx
@@ -0,0 +1,78 @@
+import { createIcon } from "@chakra-ui/icons";
+
+export const GiftBox = createIcon({
+  displayName: "GiftBox",
+  viewBox: "0 0 90 90",
+  // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
+  path: (
+    <>
+      <g clip-path="url(#clip0_128_1379)">
+        <path
+          d="M76.7646 90.0001H13.2352C11.7721 90.0001 10.5881 88.8162 10.5881 87.353V45.0001C10.5881 43.537 11.7721 42.353 13.2352 42.353H76.7646C78.2277 42.353 79.4117 43.537 79.4117 45.0001V87.353C79.4117 88.8162 78.2277 90.0001 76.7646 90.0001Z"
+          fill="url(#paint0_linear_128_1379)"
+        />
+        <path
+          d="M44.9998 26.4706C31.8628 26.4706 21.1763 15.7841 21.1763 2.64706C21.1763 1.18394 22.3602 0 23.8233 0C33.0312 0 41.0343 5.25018 44.9998 12.9148C48.9653 5.25018 56.9684 0 66.1763 0C67.6394 0 68.8233 1.18394 68.8233 2.64706C68.8233 15.7841 58.1368 26.4706 44.9998 26.4706Z"
+          fill="url(#paint1_linear_128_1379)"
+        />
+        <path
+          d="M82.0589 47.6471H7.94125C6.47813 47.6471 5.29419 46.4632 5.29419 45V23.8236C5.29419 22.3605 6.47813 21.1765 7.94125 21.1765H82.0589C83.522 21.1765 84.706 22.3605 84.706 23.8236V45C84.706 46.4632 83.522 47.6471 82.0589 47.6471Z"
+          fill="url(#paint2_linear_128_1379)"
+        />
+        <path
+          d="M52.9412 90.0001H37.0588V21.1765H52.9412V90.0001Z"
+          fill="url(#paint3_linear_128_1379)"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id="paint0_linear_128_1379"
+          x1="44.9999"
+          y1="90.0001"
+          x2="44.9999"
+          y2="42.353"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FD3A84" />
+          <stop offset="1" stop-color="#FFA68D" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_128_1379"
+          x1="44.9998"
+          y1="26.4706"
+          x2="44.9998"
+          y2="0"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FD3A84" />
+          <stop offset="1" stop-color="#FFA68D" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_128_1379"
+          x1="45.0001"
+          y1="47.6471"
+          x2="45.0001"
+          y2="21.1765"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FD3A84" />
+          <stop offset="1" stop-color="#FFA68D" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_128_1379"
+          x1="45"
+          y1="90.0001"
+          x2="45"
+          y2="21.1765"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FFC8D1" />
+          <stop offset="1" stop-color="#FFF6F7" />
+        </linearGradient>
+        <clipPath id="clip0_128_1379">
+          <rect width="90" height="90" fill="white" />
+        </clipPath>
+      </defs>
+    </>
+  ),
+});
