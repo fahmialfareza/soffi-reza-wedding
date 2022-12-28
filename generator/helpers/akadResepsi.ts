b0694e396cfdc0cda850e7b540a7e5ce77c6acf7
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-12-28 23:37:36 +0700

    finishing

--- generator/helpers/akadResepsi.ts
+++ generator/helpers/akadResepsi.ts
@@ -1,24 +1,30 @@
 export default function akadResepsi(name: string, link: string) {
-  const text = `Assalamu’alaikum wr wb ✨
+  const text = `بِسْـــــــــــمِ اللهِ الرَّحْمَنِ الرَّحِيْـــــــــــــم
 
-Tanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i *${name}* untuk menghadiri acara kami
+_Assalamu'alaikum Warahmatullahi Wabarakatuh_
 
-Akad & Resepsi
+Maha suci Allah yang telah menjadikan segala sesuatu lebih indah dan sempurna. 
+
+Tanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i Ade Faisal untuk menghadiri acara pernikahan kami
+
+*Akad & Resepsi*
 🗓 Sabtu, 14 Januari 2023
-⏰ Pukul 08.00 WIB s.d Selesai
+⏰ Pukul 07.00 WIB s.d selesai
 🏠 Bandung RT 02/RW 01, Beji, Tulung, Klaten
 
-Berikut link undangan kami, untuk info lengkap dari acara bisa kunjungi :
+Informasi lengkap mengenai acara terdapat pada *link undangan berikut*:
 
 ${link}
 
-Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.
+Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu. Mohon maaf perihal undangan hanya kami bagikan melalui pesan ini. 
 
-Dan karena suasana masih pandemi, diharapkan untuk tetap menggunakan masker dan datang pada jam yang telah ditentukan.
+Terima kasih.
+_Wassalamu'alaikum Warahmatullahi Wabarakatuh_
 
-Terima kasih banyak atas perhatiannya.
+====
 
-Wassalamu’alaikum wr wb ✨`;
+Kami yang berbahagia,
+*Soffi & Reza*`;
 
   return text;
 }
